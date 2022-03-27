2a356107b485163b81a5e7ce29b6eb266c7b7b99
RyutaGoto <b1016230@fun.ac.jp>
2022-03-28 00:20:25 +0900

    updated root

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import { LiveReload, Outlet } from "@remix-run/react";
+import { Links, LiveReload, Outlet } from "@remix-run/react";
 
 export default function App() {
   return (
@@ -6,11 +6,11 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <title>Remix: So great, it's funny!</title>
+        <Links />
       </head>
       <body>
-        Hello world
-        <LiveReload />
         <Outlet />
+        <LiveReload />
       </body>
     </html>
   );
